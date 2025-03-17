831f4e9c82669c587d4243f9e2db3bc34586af9f
jscheth <sovichea.chet@gmail.com>
2025-03-17 00:10:15 -0700

    Initial Commit

---
+++ I Am PigTests/I_Am_PigTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_PigTests.swift
+//  I Am PigTests
+//
+//  Created by Jonathan Cheth on 3/17/25.
+//
+
+import Testing
+@testable import I_Am_Pig
+
+struct I_Am_PigTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
